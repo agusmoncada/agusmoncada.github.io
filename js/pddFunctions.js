a9b29e999e480b10744aa7d6f13ebf35a271e445
agusmoncada <agus.monc@gmail.com>
2016-02-23 15:18:08 -0300

    Pueblo de Dios

--- js/pddFunctions.js
+++ js/pddFunctions.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
 
+	$('.home').scroll(function(){
+		if ($(this).scrollTop() > 1530) {
+			$('.scrollToTop').addClass( 'fadein' );
+		} else {
+			$('.scrollToTop').removeClass( 'fadein' );
+		}
+	});
+
+	$('.scrollToTop').click(function(){
+		$('html, body, .home').animate({scrollTop : 0},800);
+		return false;
+	});
+
+});
+
+$(document).ready(function(){
+
   $('.head--link').click(function(e){
 
     var linkHrer = $(this).attr('href');
