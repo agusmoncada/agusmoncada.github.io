b6cab2fdc3e9a24cd5253220b3cc888b3da2b16d
agusmoncada <agus.monc@gmail.com>
2016-03-06 14:32:10 -0300

    Pueblo de Dios

--- js/pddFunctions.js
+++ js/pddFunctions.js
@@ -1,5 +1,6 @@
 
 
+//boton derecho abajo para ir hacia arriba
 $(document).ready(function(){
 
 	$('.home').scroll(function(){
@@ -17,6 +18,7 @@ $(document).ready(function(){
 
 });
 
+//autoscroll
 $(document).ready(function(){
 
   $('.head--link').click(function(e){
@@ -29,4 +31,44 @@ $(document).ready(function(){
 
     e.preventDefault();
   });
+});
+
+//Aparece el boton cuando muevo el mouse sobre el video
+$(document).ready(function(){
+
+	var $btn = $('.btn');
+	var $videoC = $('.video');
+
+	$videoC.on('mouseenter', function(){
+		$('.btn').addClass('showBttn');
+	});
+
+	$videoC.on('mouseout', function(){
+		$('.btn').removeClass('showBttn');
+	});
+
+	$btn.on('mouseenter', function(){
+		$('.btn').addClass('showBttn');
+	});
+
+});
+
+//Reproducir y parar video + cambiar el icono
+$(document).ready(function(){
+
+	var $btn = $('.btn');
+	var vidFunc = $('.vidFunc').get(0);
+
+	$btn.on('click', function(){
+
+		if (vidFunc.paused) {
+			vidFunc.play();
+			$('.btn').addClass('pause');
+		} else {
+			vidFunc.pause();
+			$('.btn').removeClass('pause');
+		}
+
+	});
+
 });
