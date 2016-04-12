8196352c289bbe28a7c2252219b2427e76edefaa
agusmoncada <agus.monc@gmail.com>
2016-04-12 10:44:15 -0300

    agus mocada

--- ScrollRight/function.js
+++ ScrollRight/function.js
@@ -1,15 +1,13 @@
 
 $(document).ready(function(){
 
+  var ua = navigator.userAgent.toLowerCase();
   var $titleScroll = $('.titleScroll');
   var $window = $(window);
   var $paragraph = $('.paraScrollText');
   var $whiteWork = $('.whiteWork');
   var $workScroll = $('.workScroll');
   var $workContainer = $('.workContainer');
-
-
-
 
   $window.on('scroll', function() {
     var scrolledTop = $window.scrollTop() * 4;
@@ -18,22 +16,37 @@ $(document).ready(function(){
     var opacity3 = -($window.scrollTop() - 4300) / $window.scrollTop();
     var scrollWork = scrolledTop - 8000;
 
-    if ($window.scrollTop() > -1) {
-      $titleScroll.css({'transform': 'translate3d(-' + scrolledTop + 'px, 0px, 0px)', 'opacity': opacity});
+    if(ua.indexOf('applewebkit') >= 0 && ua.indexOf('chrome') < 0) {
+      //with 2d transform for safari smooth scrolling
+      if ($window.scrollTop() > -1) {
+        $titleScroll.css({'transform': 'translate(-' + scrolledTop + 'px, 0px)', 'opacity': opacity});
 
-      $paragraph.css({'opacity': -opacity2});
-  }
+        $paragraph.css({'opacity': -opacity2});
+    }
 
-    if (scrolledTop >= 8400) {
-      $whiteWork.addClass('in-view');
-      $workScroll.css({'opacity': opacity3});
-      $workContainer.css({'transform': 'translate3d(-' + scrollWork + 'px, 0px, 0px)'});
+      if (scrolledTop >= 8400) {
+        $whiteWork.addClass('in-view');
+        $workScroll.css({'opacity': opacity3});
+        $workContainer.css({'transform': 'translate(-' + scrollWork + 'px, 0px)'});
+      } else {
+        $whiteWork.removeClass('in-view');
+      }
+      //with 3d transform for other browsers smooth scrolling
     } else {
-      $whiteWork.removeClass('in-view');
+      if ($window.scrollTop() > -1) {
+        $titleScroll.css({'transform': 'translate3d(-' + scrolledTop + 'px, 0px, 0px)', 'opacity': opacity});
+
+        $paragraph.css({'opacity': -opacity2});
+    }
+
+      if (scrolledTop >= 8400) {
+        $whiteWork.addClass('in-view');
+        $workScroll.css({'opacity': opacity3});
+        $workContainer.css({'transform': 'translate3d(-' + scrollWork + 'px, 0px, 0px)'});
+      } else {
+        $whiteWork.removeClass('in-view');
+      }
     }
 
   });
-
-
-
 });
